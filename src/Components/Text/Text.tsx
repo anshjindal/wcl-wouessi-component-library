3e4cfb982265f3d67978dacba204b4f76f98d60c
vrundacvaghasiya <vrundacvaghasiya@gmail.com>
2025-03-12 11:02:32 -0400

    Task:86a779ct4 - Add 'children' prop to Text component and render content inside <p> tags

--- src/Components/Text/Text.tsx
+++ src/Components/Text/Text.tsx
@@ -1,7 +1,13 @@
 import "./Text.module.scss";
+import { ReactNode } from 'react';
 
-const Text = () => {
-  return <p className="text">empty paragraph for now</p>;
+
+const Text = ({ children }: TextProps) => {
+  return <p className="text">{children}</p>;
 };
+
+export type TextProps = {
+  children: ReactNode;
+}
 
 export default Text;

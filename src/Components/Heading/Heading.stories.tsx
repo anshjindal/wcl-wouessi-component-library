5a25de57f475b08f4f2b06dcd68ab458ebb10573
Saja Elkurtehi <114425874+Saja-elkurtehi@users.noreply.github.com>
2025-03-26 08:07:50 -0400

    fixed heading. added documentation and ensured everything was properly working on Storybook.

--- src/Components/Heading/Heading.stories.tsx
+++ src/Components/Heading/Heading.stories.tsx
@@ -5,11 +5,14 @@ const meta: Meta<typeof Heading> = {
   title: 'Components/Heading',
   component: Heading,
   argTypes: {
-    children: {control: 'text'},
+    children: {
+      control: 'text',
+      description: 'Content of the heading'
+    },
     level: {
       control:'select',
       options: [1, 2, 3, 4, 5, 6, 7],
-      description: 'Heading level.',
+      description: 'Heading level. Levels 1 to 6 render level 7 renders a <p tag.',
     },
   },
 };
